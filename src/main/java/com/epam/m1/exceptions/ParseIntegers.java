23495b6f899bbe67fae9d0792c613c52eee6ae84
usrnameuser <150504703+usrnameuser@users.noreply.github.com>
2024-07-02 14:00:47 +0300

    Update ParseIntegers.java

--- src/main/java/com/epam/m1/exceptions/ParseIntegers.java
+++ src/main/java/com/epam/m1/exceptions/ParseIntegers.java
@@ -10,8 +10,11 @@ import java.util.List;
  */
 public class ParseIntegers {
 
-    private static final List<String> WORDS = Arrays.asList("JDK 17 has released on 14 September 2021 with 10 new features, 2 feature removals and 2 feature deprecations.".split(" "));
-
+    private static final List<String> WORDS = 
+                Arrays.asList(
+                    "JDK 17 has released on 14 September 2021 with 10 new features, 2 feature removals and 2 feature deprecations."
+                            .split(" "));
+    
     public static void main(String[] args) {
         Iterator<String> words = WORDS.iterator();
         int sum = 0;
