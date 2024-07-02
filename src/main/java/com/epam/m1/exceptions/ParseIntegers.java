56c697feee221bc70cb0f4cb242a6648255cc559
usrnameuser <150504703+usrnameuser@users.noreply.github.com>
2024-07-02 14:37:19 +0300

    Update ParseIntegers.java

--- src/main/java/com/epam/m1/exceptions/ParseIntegers.java
+++ src/main/java/com/epam/m1/exceptions/ParseIntegers.java
@@ -30,7 +30,7 @@ public class ParseIntegers {
             }
         }
         System.out.println("Sum is " + sum);
-        System.out.println("Just words:" + justWords);
+        System.out.println("Just words:" + sum);
     }
 }
 
